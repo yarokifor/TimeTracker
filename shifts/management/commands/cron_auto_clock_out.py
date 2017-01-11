08ea5e5b3327b0a39fe45e7d4c429e9c69631ce4
Yaro Kifor <ykifor@netsville.com>
2017-01-11 10:10:32 -0500

    Fixed crontab command for auto clock out

--- shifts/management/commands/cron_auto_clock_out.py
+++ shifts/management/commands/cron_auto_clock_out.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
 from shifts.models import Shift, Event, Profile
+import datetime
 
 class Command(BaseCommand):
     help = "This is for crontab, so the auto clock out feature will work"
@@ -8,13 +10,14 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-       now = datetime.datetime.now()
-        users = User.objects.exclude(profile__auto_clock_out__isnull = False, profile__auto_clock_out__gte = now.time())
+        now = datetime.datetime.now()
+        users = User.objects.exclude(profile__auto_clock_out__isnull = True)
+        users = users.filter(profile__auto_clock_out__lte = now.time()) 
         for user in users:
             clock_out_event = Event(
-                time = datetime.datetime.combine(now.date, user.profile.auto_clock_out)
-                event = "OUT"
-                user = user
+                time = datetime.datetime.combine(now.date, user.profile.auto_clock_out),
+                event = "OUT",
+                user = user,
             )
             clock_out_event.save()
             last_shift = Shifts.objects.filter(user = user, end = None)
